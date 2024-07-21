b744ce321c4f497da3a93baeb91522a8b6e6afc5
Code-Warlock <dessymichaeltgp123>
2024-07-21 14:16:32 +0100

    Loops in JS and mini challenge

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+// Change(swap) the content of variables
+
+// let x = parseInt(prompt("Enter your value for x"));
+// let y = parseInt(prompt("Enter your value for y"));
+// console.log("The value of x was", x)
+// console.log("The value of y was", y)
+
+// let add = x + y;
+
+// x = add - x; // y
+// y = add - y // x
+
+// // tagx = oga
+// // tagy = fm
+// // tagt = ogafm
+// // tagx = tagy(tagt - tagx)
+// // tagy = tagx(tagt - tagy)
+
+
+// console.log("The value of x is now", x)
+// console.log("The value of y is now", y)
+
+let x = prompt("Enter your value for x");
+let y = prompt("Enter your value for y");
+console.log("The value of x was", x)
+console.log("The value of y was", y)
+
+let temp = x; // 23
+x = y; // 45
+y = temp; // 23
+
+console.log("The value of x is now", x)
+console.log("The value of y is now", y)
---
+++ assets/js/loops.js
@@ -0,0 +1,32 @@
+// Decrement/Increment Operators(assignment operators contd)
+
+// let x = 10;
+
+// console.log(++x); // 
+
+// console.log(x); // 11
+
+// ++(suffix) ====> slow increment(x++)*only assigns and updates later
+// ++(prefix) ====> fast increment(++x)*assigns and updates at once
+
+// --(suffix) ====> slow decrement(x--)*only assigns and updates later
+// --(prefix) ====> fast decrement(--x)*assigns and updates at once
+
+
+// =========================================================
+// for loops
+// while loops
+
+// Normal(Default for loop)
+// Assigment 1 ==> find odds
+
+// for(let i = 1;i<=50;i++){
+//     console.log(i);
+// }
+
+// let user = "Qazeem Adeboye"; 
+
+// for(let i = 0;i<=user.length-1;i++){
+//     console.log(user.charAt(i));
+// }
+
